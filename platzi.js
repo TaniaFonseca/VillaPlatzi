a8fe9a0222e0866d6e833430b5f10df2a8a911bf
Tania Fonseca Garcia <122373158+TaniaFonseca@users.noreply.github.com>
2023-04-11 19:45:15 -0500

    clase 2

--- platzi.js
+++ platzi.js
@@ -1,22 +1,75 @@
 var vp = document.getElementById("villaplatzi");
 var papel = vp.getContext("2d");
-var mapa = "mapa.png";
 
-var fondo = new Image(); 
-fondo.src = mapa;
-fondo.addEventListener("load",dibujar);
+var fondo = {
+    URL:"mapa.png",
+    cargaOK: false
+}
 
-var vaca = new Image(); 
-vaca.src = "vaca.png";
-vaca.addEventListener("load",dibujar);
+var vaca = {
+    URL:"vaca.png",
+    cargaOK: false
+}
 
-var vaca = new Image(); 
-vaca.src = "vaca.png";
-vaca.addEventListener("load",dibujar);
+var cerdo = {
+    URL:"cerdo.png",
+    cargaOK: false
+}
 
+var pollo = {
+    URL:"pollo.png",
+    cargaOK: false
+}
+
+fondo.objeto = new Image(); 
+fondo.objeto.src = fondo.URL;
+fondo.objeto.addEventListener("load",cargarFondo);
+
+vaca.objeto = new Image(); 
+vaca.objeto.src = vaca.URL ;
+vaca.objeto.addEventListener("load",cargarVacas);
+
+cerdo.objeto = new Image(); 
+cerdo.objeto.src = cerdo.URL ;
+cerdo.objeto.addEventListener("load",cargarCerdos);
+
+pollo.objeto = new Image(); 
+pollo.objeto.src = pollo.URL;
+pollo.objeto.addEventListener("load",cargarPollos);
+
+function cargarFondo ()
+{
+    fondo.cargaOK = true
+    dibujar ();
+}
+
+function cargarVacas ()
+{
+    vaca.cargaOK = true
+    dibujar();
+}
+
+function cargarCerdos ()
+{
+    cerdo.cargaOK = true
+    dibujar ();
+}
+
+function cargarPollos ()
+{
+    pollo.cargaOK = true
+    dibujar ();
+}
 
 function dibujar()
 {
-    papel.drawImage(fondo,0,0);
+if(fondo.cargaOK)
+  {
+    papel.drawImage(fondo.objeto,0,0)
+  }
+  if(vaca.cargaOK)
+  {
+    papel.drawImage(vaca.objeto,100,100)
+  }
 }
 
